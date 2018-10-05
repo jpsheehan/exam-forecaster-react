f1916c595862ccbf755657a0279ec9c278bf4f9a
Jesse Sheehan <jesse@sheehan.nz>
2018-10-06 11:26:05 +1300

    Fixed the navbar link. Removed the favicon. Changed the title.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper blue">
-      <a href="/" className="brand-logo center">Exam Forecaster</a>
+      <Link to="/" className="brand-logo center">Exam Forecaster</Link>
     </nav>
   );
 };
