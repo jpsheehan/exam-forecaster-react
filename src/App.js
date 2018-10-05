3004e0b4897d0a01d8a276cc64ceb2730b841634
Jesse Sheehan <jesse@sheehan.nz>
2018-10-06 11:13:47 +1300

    Added copyright notice.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,16 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/:course_code" component={Course} />
           </Switch>
+          <footer class="page-footer">
+            <div className="container">
+              <p>Disclaimer: I make no guarantees or claims that the information represented by this application is accurate.</p>
+            </div>
+            <div className="footer-copyright">
+              <div className="container">
+                Copyright &copy; 2018 JP Sheehan
+              </div>
+            </div>
+          </footer>
         </div>
       </BrowserRouter>
     );
